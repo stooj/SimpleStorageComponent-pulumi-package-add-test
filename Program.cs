2608412609bcfac0e396685f216fd9a993474927
stoo johnston <stoo@pulumi.com>
2025-09-08 10:52:43 +0100

    disable program entry point

--- Program.cs
+++ Program.cs
@@ -2,25 +2,25 @@ using System.Collections.Generic;
 using Pulumi;
 using PulumiSampleComponent;
 
-return await Deployment.RunAsync(() =>
-{
-    // Create an instance of our custom component
-    var storageComponent = new SimpleStorageComponent("my-storage-component", new SimpleStorageComponentArgs
-    {
-        StorageAccountName = "mystorageacct12345", // Must be globally unique
-        Location = "East US",
-        Tags = new InputMap<string>
-        {
-            ["Environment"] = "Development",
-            ["Project"] = "PulumiSample"
-        }
-    });
-
-    // Export some values
-    return new Dictionary<string, object?>
-    {
-        ["resourceGroupName"] = storageComponent.ResourceGroup.Apply(rg => rg.Name),
-        ["storageAccountName"] = storageComponent.StorageAccount.Apply(sa => sa.Name),
-        ["primaryBlobEndpoint"] = storageComponent.PrimaryEndpoint
-    };
-});
+// return await Deployment.RunAsync(() =>
+// {
+//     // Create an instance of our custom component
+//     // var storageComponent = new SimpleStorageComponent("my-storage-component", new SimpleStorageComponentArgs
+//     // {
+//     //     StorageAccountName = "mystorageacct12345", // Must be globally unique
+//     //     Location = "East US",
+//     //     Tags = new InputMap<string>
+//     //     {
+//     //         ["Environment"] = "Development",
+//     //         ["Project"] = "PulumiSample"
+//     //     }
+//     // });
+// 
+//     // Export some values
+//     return new Dictionary<string, object?>
+//     {
+//         ["resourceGroupName"] = storageComponent.ResourceGroup.Apply(rg => rg.Name),
+//         ["storageAccountName"] = storageComponent.StorageAccount.Apply(sa => sa.Name),
+//         ["primaryBlobEndpoint"] = storageComponent.PrimaryEndpoint
+//     };
+// });
